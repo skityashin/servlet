9a8e936d947eb68d10c0a9ced8c20eba441fd89f
Skityashin <skityashin@gmail.com>
2015-11-21 12:33:37 +0200

    12

--- src/main/java/com/levelup/servlet/MainServlet.java
+++ src/main/java/com/levelup/servlet/MainServlet.java
@@ -1,29 +1,23 @@
 package com.levelup.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+@WebServlet("/")
 public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
-        PrintWriter out = resp.getWriter();
-        out.print("<h1>Hello Servlet</h1>");
-    }
+        req.setAttribute("name", "Skityashin");
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+        req.getRequestDispatcher("mypage.jsp").forward(req, resp);
 
-        super.doPost(req, resp);
     }
 
 }
-
-
---
+++ src/main/webapp/mypage.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: User
+  Date: 21.11.2015
+  Time: 12:15
+
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Skityashin.com</title>
+</head>
+<body>
+    <h1>Hello ${name}</h1>
+</body>
+</html>
